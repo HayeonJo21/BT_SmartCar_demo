d9c447521b86b9df138d72ab02f609be12312a7b
HayeonJo21 <gkdus6629@gmail.com>
2022-02-09 17:36:05 +0900

    Initial Commit

---
+++ BT_SmartCar_demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BT_SmartCar_demo
+//
+//  Created by Norma on 2022/02/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
