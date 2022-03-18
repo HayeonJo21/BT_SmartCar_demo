80352565e4b39e44719765eaf70ca915a69481d5
HayeonJo21 <gkdus6629@gmail.com>
2022-03-18 16:54:45 +0900

    ControlView 화면 구성

---
+++ BT_SmartCar_demo/ControlViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ControlViewController.swift
+//  
+//
+//  Created by Norma on 2022/03/18.
+//
+
+import UIKit
+
+class ControlViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: (UIImage(named: "dpbgblue_00")!))
+    }
+
+}
